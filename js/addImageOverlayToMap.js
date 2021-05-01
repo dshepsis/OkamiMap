813d75751678941405ecf6b3551dff8a06c65f69
Sim <me@zhongyuen.dev>
2021-05-01 13:36:39 +0800

    Revert "Modularical"
    This reverts commit 42b8be43dd9f4209a4370345ffd07e2981dd81b0.

--- js/addImageOverlayToMap.js
+++
@@ -1,3 +0,0 @@
-export default function (mapIDHex, bounds, map) {
-  L.imageOverlay(`./DumpedMaps/map_r${mapIDHex}_0out.png`, bounds).addTo(map)
-}
--- js/paraFetchJson.js
+++
@@ -1,7 +0,0 @@
-/* Fetch multiple JSON files in parallel */
-export default async function (...URLs) {
-  const requests = URLs.map(u=>
-    fetch(u).then(r=>r.json())
-  );
-  return Promise.all(requests);
-}
